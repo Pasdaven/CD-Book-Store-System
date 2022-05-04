6dc48fd48bc1ece6ed363e18ae4645a22a073e40
ridemountainpig <lin0905580802@gmail.com>
2022-05-04 20:09:57 +0800

    feat : convenient store link return store data
    新增選取超商連結，post回傳店鋪資料

---
+++ convenienceStoreLink.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="https://emap.presco.com.tw/c2cemap.ashx?eshopid=870&&servicetype=1&url=http://localhost/CD-BOOK-STORE-SYSTEM/convenienceStoreLink.php">link</a>
+    <?php
+    $store_data = $_POST;
+    if (count($store_data) != 0) {
+        print_r($store_data);
+    } 
+    ?>
+</body>
+</html>
