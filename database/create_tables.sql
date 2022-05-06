e7bd4545408d2e869cb2c8a2fd3ff0d747941455
Pascal <a306090603@gmail.com>
2022-05-07 02:01:01 +0800

    table

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,42 +1,41 @@
-CREATE TABLE doctor (
-  doc_id INT UNSIGNED PRIMARY KEY comment '醫生編號',
-  id_num VARCHAR(10) NOT NULL UNIQUE comment '身分證',
-  doc_name VARCHAR(40) comment '姓名',
-  sex ENUM('Male', 'Female') comment '性別',
-  birth DATE comment '生日',
-  phone_num INT UNSIGNED comment '手機號碼',
-  doc_state ENUM ('working', 'quit', 'fire') NOT NULL DEFAULT 'working' comment '工作情況'
-) comment '醫生';
+CREATE TABLE order (
+	order_id INT UNSIGNED PRIMARY KEY comment '訂單編號',
+	member_id INT UNSIGNED NOT NULL UNIQUE comment '會員編號',
+	product_id INT UNSIGNED NOT NULL UNIQUE comment '商品編號',
+	coupon_id INT UNSIGNED NOT NULL UNIQUE comment '折價卷編號',
+	count_id INT UNSIGNED NOT NULL UNIQUE comment '數量記數',
+	deliver_method ENUM('home delivery', 'convenience store delivery') NOT NULL comment '運送方式',
+	price INT comment '價格',
+	phone_num INT UNSIGNED comment '手機號碼',
+	convenience_store INT UNSIGNED comment '便利商店',
+	address VARCHAR(100) comment '地址',
+	payment ENUM('cash', 'credit card') NOT NULL comment '付款方式',
+	state ENUM('wait', 'finish', 'cancel', 'return') NOT NULL comment '訂單狀態',
+	CONSTRAINT member_id FOREIGN KEY(member_id) REFERENCES member (member_id),
+	CONSTRAINT product_id FOREIGN KEY(product_id) REFERENCES product (product_id),
+	CONSTRAINT coupon_id FOREIGN KEY(coupon_id) REFERENCES coupon (coupon_id),
+	CONSTRAINT count_id FOREIGN KEY(count_id) REFERENCES count_record (count_id)
+) comment '訂單';
 
-CREATE TABLE allergy_list (
-  case_id INT UNSIGNED NOT NULL COMMENT '病例號碼',
-  allergy_med_id INT UNSIGNED COMMENT '過敏藥物',
-  CONSTRAINT allergy_med_id FOREIGN KEY(allergy_med_id) REFERENCES medicine (med_id),
-  CONSTRAINT allergy_list_case_id FOREIGN KEY(case_id) REFERENCES patient (case_id),
-  PRIMARY KEY(case_id,allergy_med_id)
-) COMMENT '病人過敏藥物';
+CREATE TABLE cart (
+	cart_id INT UNSIGNED PRIMARY KEY COMMENT '購物車編號',
+	member_id INT UNSIGNED NOT NULL UNIQUE comment '會員編號',
+	product_id INT UNSIGNED NOT NULL UNIQUE comment '商品編號',
+	count_id INT UNSIGNED NOT NULL UNIQUE comment '數量記數',
+	CONSTRAINT member_id FOREIGN KEY(member_id) REFERENCES member (member_id),
+	CONSTRAINT product_id FOREIGN KEY(product_id) REFERENCES product (product_id),
+	CONSTRAINT count_id FOREIGN KEY(count_id) REFERENCES count_record (count_id)
+) COMMENT '購物車';
 
-CREATE TABLE patient_records (
-  record_id INT UNSIGNED PRIMARY KEY auto_increment comment '看診紀錄編號',
-  case_id INT UNSIGNED NOT NULL comment '病歷號碼',
-  doc_id INT UNSIGNED NOT NULL comment '醫生編號',
-  consulation_date DATE comment '看診日期',
-  disease_name VARCHAR(20) comment '疾病名稱',
-  med_days INT UNSIGNED comment '用藥天數',
-  comment VARCHAR(100) NULL comment '備註',
-  CONSTRAINT case_id FOREIGN KEY(case_id) REFERENCES patient (case_id) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT patient_records_doc_id FOREIGN KEY(doc_id) REFERENCES doctor (doc_id)
-) comment '病歷';
+CREATE TABLE count_records (
+	count_id INT UNSIGNED PRIMARY KEY comment '數量記數',
+	product_id INT UNSIGNED NOT NULL UNIQUE comment '商品編號',
+	count_num INT UNSIGNED NOT NULL comment '商品數量',
+	CONSTRAINT product_id FOREIGN KEY(product_id) REFERENCES product (product_id),
+) comment '數量紀錄';
 
-CREATE TABLE member (
-  id INT UNSIGNED PRIMARY KEY auto_increment comment '會員編號',
-  member_name VARCHAR(20) comment '會員名稱',
-  birthday DATE comment '生日',
-  phone_num INT UNSIGNED comment '手機號碼',
-  sex ENUM('Male', 'Female') comment '性別',
-  cedit_num INT comment '信用分數'
-) comment '會員資料'
-
-CREATE TABLE member_account (
-  
-)
+CREATE TABLE ad (
+	ad_id INT UNSIGNED PRIMARY KEY comment '廣告編號',
+	ad_description VARCHAR(100) comment '廣告說明',
+	ad_img_id VARCHAR(100) comment '廣告圖片'
+) comment '廣告';
