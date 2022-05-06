05fd319a667e6deb15b33b39c659105fbcd4463d
ridemountainpig <lin0905580802@gmail.com>
2022-05-07 01:58:09 +0800

    feat : create table

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,35 +1,5 @@
-CREATE TABLE doctor (
-  doc_id INT UNSIGNED PRIMARY KEY comment '醫生編號',
-  id_num VARCHAR(10) NOT NULL UNIQUE comment '身分證',
-  doc_name VARCHAR(40) comment '姓名',
-  sex ENUM('Male', 'Female') comment '性別',
-  birth DATE comment '生日',
-  phone_num INT UNSIGNED comment '手機號碼',
-  doc_state ENUM ('working', 'quit', 'fire') NOT NULL DEFAULT 'working' comment '工作情況'
-) comment '醫生';
-
-CREATE TABLE allergy_list (
-  case_id INT UNSIGNED NOT NULL COMMENT '病例號碼',
-  allergy_med_id INT UNSIGNED COMMENT '過敏藥物',
-  CONSTRAINT allergy_med_id FOREIGN KEY(allergy_med_id) REFERENCES medicine (med_id),
-  CONSTRAINT allergy_list_case_id FOREIGN KEY(case_id) REFERENCES patient (case_id),
-  PRIMARY KEY(case_id,allergy_med_id)
-) COMMENT '病人過敏藥物';
-
-CREATE TABLE patient_records (
-  record_id INT UNSIGNED PRIMARY KEY auto_increment comment '看診紀錄編號',
-  case_id INT UNSIGNED NOT NULL comment '病歷號碼',
-  doc_id INT UNSIGNED NOT NULL comment '醫生編號',
-  consulation_date DATE comment '看診日期',
-  disease_name VARCHAR(20) comment '疾病名稱',
-  med_days INT UNSIGNED comment '用藥天數',
-  comment VARCHAR(100) NULL comment '備註',
-  CONSTRAINT case_id FOREIGN KEY(case_id) REFERENCES patient (case_id) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT patient_records_doc_id FOREIGN KEY(doc_id) REFERENCES doctor (doc_id)
-) comment '病歷';
-
 CREATE TABLE member (
-  id INT UNSIGNED PRIMARY KEY auto_increment comment '會員編號',
+  member_id INT UNSIGNED PRIMARY KEY auto_increment comment '會員編號',
   member_name VARCHAR(20) comment '會員名稱',
   birthday DATE comment '生日',
   phone_num INT UNSIGNED comment '手機號碼',
@@ -38,5 +8,37 @@ CREATE TABLE member (
 ) comment '會員資料'
 
 CREATE TABLE member_account (
-  
-)
+  member_id INT UNSIGNED NOT NULL comment '會員編號',
+  email VARCHAR(100) NOT NULL comment '信箱',
+  member_password VARCHAR(100) NOT NULL comment '會員密碼',
+  CONSTRAINT member_id FOREIGN KEY(member_id) REFERENCES member (member_id),
+  PRIMARY KEY(member_id, email)
+) comment '會員帳號密碼'
+
+CREATE TABLE browsing_history (
+  member_id INT UNSIGNED NOT NULL comment '會員編號',
+  product_id INT UNSIGNED NOT NULL comment '商品編號',
+  browse_time DATETIME NOT NULL comment '瀏覽時間',
+  CONSTRAINT member_id FOREIGN KEY (member_id) REFERENCES member (member_id),
+  CONSTRAINT product_id FOREIGN KEY (product_id) REFERENCES product (product_id),
+  PRIMARY KEY (member_id, product_id)
+) comment '瀏覽紀錄'
+
+CREATE TABLE follow_list (
+  member_id INT UNSIGNED NOT NULL comment '會員編號',
+  product_id INT UNSIGNED NOT NULL comment '商品編號',
+  CONSTRAINT member_id FOREIGN KEY (member_id) REFERENCES member (member_id),
+  CONSTRAINT product_id FOREIGN KEY (product_id) REFERENCES product (product_id),
+  PRIMARY KEY (member_id, product_id)
+) comment '關注清單'
+
+CREATE TABLE comment_list (
+  member_id INT UNSIGNED NOT NULL comment '會員編號',
+  product_id INT UNSIGNED NOT NULL comment '商品編號',
+  comment_id INT UNSIGNED NOT NULL comment '評論編號',
+  product_comment VARCHAR(100) NOT NULL comment '商品評論',
+  comment_create_time DATETIME NOT NULL comment '評論時間',
+  CONSTRAINT member_id FOREIGN KEY (member_id) REFERENCES member (member_id),
+  CONSTRAINT product_id FOREIGN KEY (product_id) REFERENCES product (product_id),
+  PRIMARY KEY (member_id, product_id, comment_id)
+) comment '評論區'
