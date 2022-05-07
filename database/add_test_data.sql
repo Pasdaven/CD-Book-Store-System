573fb2b24d22442239ff525e173e16f6a6e19192
ridemountainpig <lin0905580802@gmail.com>
2022-05-07 11:36:00 +0800

    feat : add database model

---
+++ database/add_test_data.sql
@@ -0,0 +1,20 @@
+INSERT INTO `member` 
+VALUES  (NULL, 'david', '2022-05-08', '0912345345', 'Male', '100'),
+        (NULL, 'ho', '2022-01-08', '091234456', 'Male', '100'),
+        (NULL, '阿東雅', '2022-02-08', '0912456545', 'Female', '100'),
+        (NULL, '阿南懷', '2012-05-04', '0946456534', 'Male', '100'),
+        (NULL, '阿南郭', '2002-05-08', '0914789784', 'Female', '100');
+
+INSERT INTO `member_account`
+VALUES  (1, 'member1@gmail.com', '1234'),
+        (2, 'member2@gmail.com', 'qwer'),
+        (3, 'member3@gmail.com', 'asdf'),
+        (4, 'member4@gmail.com', 'zxcv'),
+        (5, 'member5@gmail.com', '4321');
+
+INSERT INTO `product` 
+VALUES  (NULL, 'book1', 'book1', '100', NULL, NULL, NULL, '200'),
+        (NULL, 'book2', 'book2', '200', NULL, NULL, NULL, '20'),
+        (NULL, 'book3', 'book3', '340', NULL, NULL, NULL, '250'),
+        (NULL, 'book4', 'book4', '1240', NULL, NULL, NULL, '232'),
+        (NULL, 'book5', 'book5', '16800', NULL, NULL, NULL, '600');
