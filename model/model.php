feccfbbdc21bb4b7347eae704f9fc746dfc4a81b
Pascal <a306090603@gmail.com>
2022-05-10 02:07:26 +0800

    model query function

--- model/model.php
+++ model/model.php
@@ -17,6 +17,46 @@ class Model {
         $result = mysqli_query($this->getDB(), $sql);
         return $result;
     }
+    public function insert($table, $line) {
+        return "INSERT INTO $table (" . implode(',', array_keys($line)) . ") VALUES (" . implode(',', $line) . ")";
+    }
+    public function update($table, $line) {
+        $str = "";
+        $i = 0;
+        foreach ($line as $kname => $kvalue) {
+            $str .= $kname . "=" . $kvalue;
+            $i++;
+            if ($i < count($line)) {
+                $str .= ",";
+            }
+        }
+        return "UPDATE $table SET " . $str;
+    }
+    public function delete($table) {
+        return "DELETE FROM $table";
+    }
+    public function select($table, $arr = NULL) {
+        if ($arr) {
+            return "SELECT " . implode(',', $arr) . " FROM $table";
+        } else {
+            return "SELECT * FROM $table";
+        }
+    }
+    public function where($kname, $comparator, $kvalue) {
+        return " WHERE $kname $comparator $kvalue";
+    }
+    public function orwhere($kname, $operator, $kvalue) {
+        return " OR $kname $operator $kvalue";
+    }
+    public function orderby($kname, $sort) {
+        return " ORDER BY $kname $sort";
+    }
+    public function naturaljoin($table) {
+        return " NATURAL JOIN $table";
+    }
+    public function leftjoin($table, $kname, $comparator, $kvalue) {
+        return " LEFT JOIN ON $kname $comparator $kvalue";
+    }
     //取整個表
     public function getAll() {
         $sql = "SELECT * FROM $this->table";
