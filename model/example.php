b10a25ab617878d62ef0b5ff90f1cbcb9b3be493
Pascal <a306090603@gmail.com>
2022-05-11 00:04:33 +0800

    feat: example file of using model

---
+++ model/example.php
@@ -0,0 +1,21 @@
+<?php
+$sql = $this->select('table') . $this->where('id', '=', '10');
+//找幾個都要打[]
+$sql = $this->select('table', ['id', 'name']) . $this->where('id', '=', '10');
+//result是陣列
+$result = $this->execute($sql);
+
+$sql = $this->insert(['price' => 400, 'product' => 'Laptop', 'prod' => 'Laptop']);
+//無回傳值
+$this->execute($sql);
+
+$sql = $this->update(['price' => 400, 'product' => 'Laptop', 'prod' => 'Laptop']);
+$sql = $this->update(['price' => 400]);
+$this->execute($sql);
+
+$sql = $this->delete('table') . $this->where('id', '=', '10');
+
+$sql = $this->select('table') . $this->where('id', '=', $this->select('othertable', ['id']));
+$sql = $this->select('table') . $this->naturaljoin('othertable');
+
+$sql = $this->select('table', ['id', 'name']) . $this->leftjoin('othertable');
