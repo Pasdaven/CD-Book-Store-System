e307202b9215e76d2469acf1d153cfad3fd86b03
ridemountainpig <lin0905580802@gmail.com>
2022-05-11 20:25:52 +0800

    feat : add ad function

--- controller/ad.php
+++ controller/ad.php
@@ -4,4 +4,14 @@ use model\Model;
 
 class Ad extends Model {
     protected $table = 'ad';
+
+    public function insertAd($param) {
+        $sql = $this->insert(['ad_description' => $param['ad_description'], 'ad_img_id' => $param['ad_img_id']]);
+        $this->execute($sql);
+    }
+
+    public function deleteAd($param) {
+        $sql = $this->delete() . $this->where('ad_id', '=', $param['ad_id']);
+        $this->execute($sql);
+    }
 }
