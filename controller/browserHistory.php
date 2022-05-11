76aad33f595a3c99187753a1f6e16559262d2b23
ridemountainpig <lin0905580802@gmail.com>
2022-05-12 02:10:51 +0800

    feat : change table, add getBrowserHis function

--- controller/browserHistory.php
+++ controller/browserHistory.php
@@ -3,10 +3,15 @@
 use model\Model;
 
 class BrowserHistory extends Model {
-    protected $table = 'browser_history';
+    protected $table = 'browsing_history';
 
     public function insertBrowserHis($param) {
         $sql = $this->insert(['member_id' => $param['member_id'], 'product_id' => $param['product_id'], 'browse_time' => $param['browse_time']]);
         $this->execute($sql);
+    }
+
+    public function getBrowserHis() {
+        $sql = $this->select($this->table);
+        return $this->execute($sql);
     }
 }
