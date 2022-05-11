40c94250f136b18b642427d15e54275cb2388bb4
Pascal <a306090603@gmail.com>
2022-05-12 00:56:39 +0800

    combine comment function

--- controller/cart.php
+++ controller/cart.php
@@ -4,4 +4,29 @@ use model\Model;
 
 class Cart extends Model {
     protected $table = 'cart';
+    public function insertCart($param) {
+        $cart_id = $param['cart_id'];
+        $member_id = $param['member_id'];
+        $product_id = $param['product_id'];
+        $count_id = $param['count_id'];
+        $sql = $this->insert(['member_id' => $member_id, 'product_id' => $product_id, 'cart_id' => $cart_id, 'count_id' => $count_id]);
+        return $this->execute($sql);
+    }
+    public function updateCart($param) {
+        $comment_id = $param['comment_id'];
+        $product_comment = $param['product_comment'];
+        $comment_create_time = date("Y-m-d H:i:s");
+        $sql = $this->update(['product_comment' => $product_comment, 'comment_create_time' => $comment_create_time]) . $this->where('comment_id', '=', $comment_id);
+        return $this->execute($sql);
+    }
+    public function deleteCart($param) {
+        $comment_id = $param['comment_id'];
+        $sql = $this->delete() . $this->where('comment_id', '=', $comment_id);
+        echo $sql;
+        return $this->execute($sql);
+    }
+    public function getCart() {
+        $sql = $this->select($this->table);
+        return $this->execute($sql);
+    }
 }
--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,129 +1,129 @@
 CREATE TABLE member (
-    member_id INT UNSIGNED PRIMARY KEY auto_increment comment '會員編號',
-    member_name VARCHAR(20) comment '會員名稱',
-    birthday DATE comment '生日',
-    phone_num INT UNSIGNED comment '手機號碼',
-    sex ENUM('Male', 'Female') comment '性別',
-    cedit_num INT comment '信用分數'
-) comment '會員資料';
+    member_id INT UNSIGNED PRIMARY KEY auto_increment COMMENT '會員編號',
+    member_name VARCHAR(20) COMMENT '會員名稱',
+    birthday DATE COMMENT '生日',
+    phone_num INT UNSIGNED COMMENT '手機號碼',
+    sex ENUM('Male', 'Female') COMMENT '性別',
+    cedit_num INT COMMENT '信用分數'
+) COMMENT '會員資料';
 
 CREATE TABLE product (
-    product_id INT UNSIGNED PRIMARY KEY auto_increment comment '商品編號',
-    product_name VARCHAR(100) NOT NULL comment '商品名稱',
-    product_description VARCHAR(100) NOT NULL comment '商品說明',
-    product_price INT UNSIGNED NOT NULL comment '商品價格',
-    product_discount ENUM ('0.9', '0.85', '0.8') comment '商品特價',
-    product_discount_start_date DATE comment '商品折價開始日期',
-    product_discount_end_date DATE comment '商品折價開始日期',
-    product_number INT UNSIGNED NOT NULL comment '商品庫存'
-) comment '商品';
+    product_id INT UNSIGNED PRIMARY KEY auto_increment COMMENT '商品編號',
+    product_name VARCHAR(100) NOT NULL COMMENT '商品名稱',
+    product_description VARCHAR(100) NOT NULL COMMENT '商品說明',
+    product_price INT UNSIGNED NOT NULL COMMENT '商品價格',
+    product_discount ENUM ('0.9', '0.85', '0.8') COMMENT '商品特價',
+    product_discount_start_date DATE COMMENT '商品折價開始日期',
+    product_discount_end_date DATE COMMENT '商品折價開始日期',
+    product_number INT UNSIGNED NOT NULL COMMENT '商品庫存'
+) COMMENT '商品';
 
 CREATE TABLE coupon (
-    coupon_id INT UNSIGNED PRIMARY KEY auto_increment comment '券編號',
-    member_id INT UNSIGNED NOT NULL comment '會員編號',
-    feature ENUM ('50', '100', '150', 'free-shipping') NOT NULL comment '券種類',
-    coupon_month INT UNSIGNED NOT NULL comment '券月份'
-) comment '券';
+    coupon_id INT UNSIGNED PRIMARY KEY auto_increment COMMENT '券編號',
+    member_id INT UNSIGNED NOT NULL COMMENT '會員編號',
+    feature ENUM ('50', '100', '150', 'free-shipping') NOT NULL COMMENT '券種類',
+    coupon_month INT UNSIGNED NOT NULL COMMENT '券月份'
+) COMMENT '券';
 
 CREATE TABLE count_records (
-    count_id INT UNSIGNED PRIMARY KEY auto_increment comment '數量記數',
-    product_id INT UNSIGNED NOT NULL UNIQUE comment '商品編號',
-    count_num INT UNSIGNED NOT NULL comment '商品數量',
+    count_id INT UNSIGNED PRIMARY KEY auto_increment COMMENT '數量記數',
+    product_id INT UNSIGNED NOT NULL UNIQUE COMMENT '商品編號',
+    count_num INT UNSIGNED NOT NULL COMMENT '商品數量',
     FOREIGN KEY(product_id) REFERENCES product (product_id)
-) comment '數量紀錄';
+) COMMENT '數量紀錄';
 
 CREATE TABLE order_list (
-    order_id INT UNSIGNED PRIMARY KEY auto_increment comment '訂單編號',
-    member_id INT UNSIGNED NOT NULL UNIQUE comment '會員編號',
-    product_id INT UNSIGNED NOT NULL UNIQUE comment '商品編號',
-    coupon_id INT UNSIGNED NOT NULL UNIQUE comment '折價卷編號',
-    count_id INT UNSIGNED NOT NULL UNIQUE comment '數量記數',
-    deliver_method ENUM('home delivery', 'convenience store delivery') NOT NULL comment '運送方式',
-    price INT comment '價格',
+    order_id INT UNSIGNED PRIMARY KEY auto_increment COMMENT '訂單編號',
+    member_id INT UNSIGNED NOT NULL UNIQUE COMMENT '會員編號',
+    product_id INT UNSIGNED NOT NULL UNIQUE COMMENT '商品編號',
+    coupon_id INT UNSIGNED NOT NULL UNIQUE COMMENT '折價卷編號',
+    count_id INT UNSIGNED NOT NULL UNIQUE COMMENT '數量記數',
+    deliver_method ENUM('home delivery', 'convenience store delivery') NOT NULL COMMENT '運送方式',
+    price INT COMMENT '價格',
     phone_num INT UNSIGNED comment '手機號碼',
-    convenience_store INT UNSIGNED comment '便利商店',
-    order_address VARCHAR(100) comment '地址',
-    payment ENUM('cash', 'credit card') NOT NULL comment '付款方式',
-    order_state ENUM('wait', 'finish', 'cancel', 'return') NOT NULL comment '訂單狀態',
+    convenience_store INT UNSIGNED COMMENT '便利商店',
+    order_address VARCHAR(100) COMMENT '地址',
+    payment ENUM('cash', 'credit card') NOT NULL COMMENT '付款方式',
+    order_state ENUM('wait', 'finish', 'cancel', 'return') NOT NULL COMMENT '訂單狀態',
     FOREIGN KEY(member_id) REFERENCES member (member_id),
     FOREIGN KEY(product_id) REFERENCES product (product_id),
     FOREIGN KEY(coupon_id) REFERENCES coupon (coupon_id),
     FOREIGN KEY(count_id) REFERENCES count_records (count_id)
-) comment '訂單';
+) COMMENT '訂單';
 
 CREATE TABLE cart (
-    cart_id INT UNSIGNED PRIMARY KEY COMMENT '購物車編號',
-    member_id INT UNSIGNED NOT NULL UNIQUE comment '會員編號',
-    product_id INT UNSIGNED NOT NULL UNIQUE comment '商品編號',
-    count_id INT UNSIGNED NOT NULL UNIQUE comment '數量記數',
+    cart_id INT UNSIGNED PRIMARY KEY auto_increment COMMENT '購物車編號',
+    member_id INT UNSIGNED NOT NULL UNIQUE COMMENT '會員編號',
+    product_id INT UNSIGNED NOT NULL UNIQUE COMMENT '商品編號',
+    count_id INT UNSIGNED NOT NULL UNIQUE COMMENT '數量記數',
     FOREIGN KEY(member_id) REFERENCES member (member_id),
     FOREIGN KEY(product_id) REFERENCES product (product_id),
     FOREIGN KEY(count_id) REFERENCES count_records (count_id)
 ) COMMENT '購物車';
 
 CREATE TABLE ad (
-    ad_id INT UNSIGNED PRIMARY KEY auto_increment comment '廣告編號',
-    ad_description VARCHAR(100) comment '廣告說明',
-    ad_img_id VARCHAR(100) comment '廣告圖片'
-) comment '廣告';
+    ad_id INT UNSIGNED PRIMARY KEY auto_increment COMMENT '廣告編號',
+    ad_description VARCHAR(100) COMMENT '廣告說明',
+    ad_img_id VARCHAR(100) COMMENT '廣告圖片'
+) COMMENT '廣告';
 
 CREATE TABLE customer_service (
-    cs_id INT UNSIGNED PRIMARY KEY auto_increment comment '客服人員編號',
-    cs_name VARCHAR(100) NOT NULL comment '客服人員姓名'
-) comment '客服人員';
+    cs_id INT UNSIGNED PRIMARY KEY auto_increment COMMENT '客服人員編號',
+    cs_name VARCHAR(100) NOT NULL COMMENT '客服人員姓名'
+) COMMENT '客服人員';
 
 CREATE TABLE cs_record (
-    cs_record_id INT UNSIGNED PRIMARY KEY auto_increment comment '客服紀錄編號',
-    member_id INT UNSIGNED NOT NULL comment '會員編號',
-    cs_id INT UNSIGNED NOT NULL comment '客服人員編號',
+    cs_record_id INT UNSIGNED PRIMARY KEY auto_increment COMMENT '客服紀錄編號',
+    member_id INT UNSIGNED NOT NULL COMMENT '會員編號',
+    cs_id INT UNSIGNED NOT NULL COMMENT '客服人員編號',
     topic ENUM ('product', 'other') NOT NULL,
     product_id INT UNSIGNED comment '商品編號',
     FOREIGN KEY (member_id) REFERENCES member(member_id),
     FOREIGN KEY (cs_id) REFERENCES customer_service(cs_id),
     FOREIGN KEY (product_id) REFERENCES product(product_id)
-) comment '客服紀錄';
+) COMMENT '客服紀錄';
 
 CREATE TABLE cs_message (
-    message_id INT UNSIGNED auto_increment comment '訊息記錄編號',
+    message_id INT UNSIGNED auto_increment COMMENT '訊息記錄編號',
     cs_record_id INT UNSIGNED comment '客服紀錄編號',
-    msg_content VARCHAR(100) NOT NULL comment '訊息內容',
-    msg_by ENUM ('cs', 'member') NOT NULL comment '訊息建立者',
+    msg_content VARCHAR(100) NOT NULL COMMENT '訊息內容',
+    msg_by ENUM ('cs', 'member') NOT NULL COMMENT '訊息建立者',
     create_time DATETIME NOT NULL comment '訊息時間',
     FOREIGN KEY(cs_record_id) REFERENCES cs_record(cs_record_id),
     PRIMARY KEY (message_id, cs_record_id)
-) comment '客服訊息';
+) COMMENT '客服訊息';
 
 CREATE TABLE member_account (
-    member_id INT UNSIGNED NOT NULL comment '會員編號',
-    email VARCHAR(100) NOT NULL UNIQUE comment '信箱',
-    member_password VARCHAR(100) NOT NULL comment '會員密碼',
+    member_id INT UNSIGNED NOT NULL COMMENT '會員編號',
+    email VARCHAR(100) NOT NULL UNIQUE COMMENT '信箱',
+    member_password VARCHAR(100) NOT NULL COMMENT '會員密碼',
     FOREIGN KEY(member_id) REFERENCES member (member_id),
     PRIMARY KEY(member_id, email)
-) comment '會員帳號密碼';
+) COMMENT '會員帳號密碼';
 
 CREATE TABLE browsing_history (
-    member_id INT UNSIGNED NOT NULL comment '會員編號',
-    product_id INT UNSIGNED NOT NULL comment '商品編號',
-    browse_time DATETIME NOT NULL comment '瀏覽時間',
+    member_id INT UNSIGNED NOT NULL COMMENT '會員編號',
+    product_id INT UNSIGNED NOT NULL COMMENT '商品編號',
+    browse_time DATETIME NOT NULL COMMENT '瀏覽時間',
     FOREIGN KEY (member_id) REFERENCES member (member_id),
     FOREIGN KEY (product_id) REFERENCES product (product_id),
     PRIMARY KEY (member_id, product_id)
-) comment '瀏覽紀錄';
+) COMMENT '瀏覽紀錄';
 
 CREATE TABLE follow_list (
-    member_id INT UNSIGNED NOT NULL comment '會員編號',
-    product_id INT UNSIGNED NOT NULL comment '商品編號',
+    member_id INT UNSIGNED NOT NULL COMMENT '會員編號',
+    product_id INT UNSIGNED NOT NULL COMMENT '商品編號',
     FOREIGN KEY (member_id) REFERENCES member (member_id),
     FOREIGN KEY (product_id) REFERENCES product (product_id),
     PRIMARY KEY (member_id, product_id)
-) comment '關注清單';
+) COMMENT '關注清單';
 
 CREATE TABLE comment_list (
-    member_id INT UNSIGNED NOT NULL comment '會員編號',
-    product_id INT UNSIGNED NOT NULL comment '商品編號',
+    member_id INT UNSIGNED NOT NULL COMMENT '會員編號',
+    product_id INT UNSIGNED NOT NULL COMMENT '商品編號',
     comment_id INT UNSIGNED PRIMARY KEY auto_increment comment '評論編號',
-    product_comment VARCHAR(100) NOT NULL comment '商品評論',
-    comment_create_time DATETIME NOT NULL comment '評論時間',
+    product_comment VARCHAR(100) NOT NULL COMMENT '商品評論',
+    comment_create_time DATETIME NOT NULL COMMENT '評論時間',
     FOREIGN KEY (member_id) REFERENCES member (member_id),
     FOREIGN KEY (product_id) REFERENCES product (product_id)
-) comment '評論區';
+) COMMENT '評論區';
