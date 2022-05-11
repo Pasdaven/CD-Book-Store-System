3acdf3e66f531759c2e9b0947f9229a5890a7a53
ridemountainpig <lin0905580802@gmail.com>
2022-05-11 20:09:55 +0800

    feat : add followList function

--- controller/followList.php
+++ controller/followList.php
@@ -4,4 +4,14 @@ use model\Model;
 
 class FollowList extends Model {
     protected $table = 'follow_list';
+
+    public function insertFollowList() {
+        $sql = $this->insert(['member_id' => '1', 'product_id' => '1']);
+        $this->execute($sql);
+    }
+
+    public function deleteFollowList($param) {
+        $sql = $this->delete() . $this->where('member_id', '=', $param['member_id']) . ' and ' . 'product_id = ' . $param['product_id'];
+        $this->execute($sql);
+    }
 }
