f288a078b12b5400bb687b45638f4b65d71a97a4
David Ho <davidho0403@gmail.com>
2022-05-13 01:36:55 +0800

    feat: createCsRecord(), createCsMessage()
    createCsRecord(): 建立一筆客服紀錄, createCsMessage(): 建立一個客服紀錄中的某個訊息

--- controller/customerService.php
+++ controller/customerService.php
@@ -4,5 +4,24 @@ use model\Model;
 
 class CustomerService extends Model {
 
-    protected $table = 'customer_service';
+    // 建立客服紀錄，傳入member_id, cs_id, topic, product_id
+    public function createCsRecord($param) {
+        $member_id = $param['member_id'];
+        $cs_id = $param['cs_id'];
+        $topic = $param['topic'];
+        $product_id = $param['product_id'];
+        $sql = $this->insert(['member_id' => $member_id, 'cs_id' => $cs_id, 'topic' => $topic, 'product_id' => $product_id], 'cs_record');
+
+        return $this->execute($sql);
+    }
+
+    public function createCsMessage($param) {
+        $cs_record_id = $param['cs_record_id'];
+        $msg_content = $param['msg_content'];
+        $msg_by = $param['msg_by'];
+        $create_time = date("Y-m-d H:i:s");
+        $sql = $this->insert(['cs_record_id' => $cs_record_id, 'msg_content' => $msg_content, 'msg_by' => $msg_by, 'create_time' => $create_time], 'cs_message');
+
+        return $this->execute($sql);
+    }
 }
