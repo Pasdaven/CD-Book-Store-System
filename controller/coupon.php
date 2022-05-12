a3ead0ea35e20d6c06c699d5b0d7e0e9ab0ecf3d
David Ho <davidho0403@gmail.com>
2022-05-12 18:21:39 +0800

    feat: add sendFreeShippingCoupon(), getCoupon()
    sendFreeShippingCoupon(): 發送免運券, getCoupon(): 查詢某個member_id擁有的coupon

--- controller/coupon.php
+++ controller/coupon.php
@@ -1,8 +1,29 @@
 <?php
 
 use model\Model;
+require_once("member.php");
 
 class Coupon extends Model {
 
     protected $table = 'coupon';
+
+    //  發送免運券
+    public function sendFreeShippingCoupon() {
+        $member = new Member();
+
+        $memberList = $member->getMemberCredit();
+        $month = date("Y-m");
+        foreach ($memberList as $value) {
+            $sql = $this->insert(['member_id' => $value['member_id'], 'feature' => 'free-shipping', 'coupon_month' => $month]);
+            $this->execute($sql);
+        }
+    }
+
+    // 查詢某個member_id擁有的coupon
+    public function getCoupon($param) {
+        $member_id = $param['member_id'];
+        $sql = $this->select($this->table) . $this->where('member_id', '=', $member_id);
+
+        return $this->execute($sql);
+    }
 }
