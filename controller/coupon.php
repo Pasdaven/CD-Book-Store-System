f0a6128f3cca74f724bb7abf24f91a3bf5736ad5
David Ho <davidho0403@gmail.com>
2022-05-13 01:23:57 +0800

    feat: useCoupon()

--- controller/coupon.php
+++ controller/coupon.php
@@ -7,7 +7,7 @@ class Coupon extends Model {
 
     protected $table = 'coupon';
 
-    //  發送免運券
+    // 發送免運券給信用分數大於80的會員
     public function sendFreeShippingCoupon() {
         $member = new Member();
 
@@ -23,6 +23,12 @@ class Coupon extends Model {
     public function getCoupon($param) {
         $member_id = $param['member_id'];
         $sql = $this->select($this->table) . $this->where('member_id', '=', $member_id);
+
+        return $this->execute($sql);
+    }
+
+    public function useCoupon($order_id, $coupon_id) {
+        $sql = $this->update(['order_id' => $order_id]) . $this->where('coupon_id', '=', $coupon_id);
 
         return $this->execute($sql);
     }
