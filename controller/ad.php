e979a851796069c0d911fc061f6f0450c3f4c4d0
ridemountainpig <lin0905580802@gmail.com>
2022-05-13 01:08:26 +0800

    feat : add each function return

--- controller/ad.php
+++ controller/ad.php
@@ -7,12 +7,12 @@ class Ad extends Model {
 
     public function insertAd($param) {
         $sql = $this->insert(['ad_description' => $param['ad_description'], 'ad_img_id' => $param['ad_img_id']]);
-        $this->execute($sql);
+        return $this->execute($sql);
     }
 
     public function deleteAd($param) {
         $sql = $this->delete() . $this->where('ad_id', '=', $param['ad_id']);
-        $this->execute($sql);
+        return $this->execute($sql);
     }
 
     public function getAd() {
