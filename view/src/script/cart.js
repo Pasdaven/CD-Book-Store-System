2fd77ca8320b839491ff76c1e19bade7534ef57e
Pascal <a306090603@gmail.com>
2022-05-18 02:53:08 +0800

    cart view and controller

--- view/src/script/cart.js
+++ view/src/script/cart.js
@@ -1,27 +1,22 @@
-const insertCart = () => {
+const insertCart = (product_id) => {
     let member_id = 1;
-    let product_id = 4;
-    let count_num = 1;
     let data = {
         controller: 'Cart',
         method: 'insertCart',
         parameter: {
             member_id: member_id,
-            product_id: product_id,
-            count_num: count_num
+            product_id: product_id
         }
     };
     let json = JSON.stringify(data);
     $.ajax({
         url: '/CD-BOOK-STORE-SYSTEM/controller/core.php',
         method: 'POST',
-        data: json,
-        success: res => console.log(res)
+        data: json
     });
 }
 
-const deleteCart = () => {
-    let cart_id = 1;
+const deleteCart = (cart_id) => {
     let data = {
         controller: 'Cart',
         method: 'deleteCart',
@@ -33,28 +28,33 @@ const deleteCart = () => {
     $.ajax({
         url: '/CD-BOOK-STORE-SYSTEM/controller/core.php',
         method: 'POST',
-        data: json,
-        success: res => console.log(res)
+        data: json
     });
 }
 
 const getCart = () => {
+    let member_id = 1;
     let data = {
         controller: 'Cart',
-        method: 'getCart'
+        method: 'getCart',
+        parameter: {
+            member_id: member_id
+        }
     };
     let json = JSON.stringify(data);
     $.ajax({
         url: '/CD-BOOK-STORE-SYSTEM/controller/core.php',
         method: 'POST',
         data: json,
-        success: res => console.log(res)
+        async: false,
+        success: function(res) {
+            result = res;
+        }
     });
+    return result;
 }
 
-const updateCart = () => {
-    let cart_id = 1;
-    let count_num = 60;
+const updateCart = (cart_id, count_num) => {
     let data = {
         controller: 'Cart',
         method: 'updateCart',
@@ -67,8 +67,7 @@ const updateCart = () => {
     $.ajax({
         url: '/CD-BOOK-STORE-SYSTEM/controller/core.php',
         method: 'POST',
-        data: json,
-        success: res => console.log(res)
+        data: json
     });
 }
 
@@ -102,10 +101,114 @@ const checkout = () => {
     });
 }
 
-$(() => {
-    getCart();
-});
-$('#insertCart').click(() => {insertCart()});
-$('#deleteCart').click(() => {deleteCart()});
-$('#updateCart').click(() => {updateCart()});
-$('#checkout').click(() => {checkout()});
+const getCoupon = () => {
+    let member_id = 1;
+    let data = {
+        controller: 'Coupon',
+        method: 'getCoupon',
+        parameter: {
+            member_id: member_id
+        }
+    };
+    let json = JSON.stringify(data);
+    $.ajax({
+        url: '/CD-BOOK-STORE-SYSTEM/controller/core.php',
+        method: 'POST',
+        data: json,
+        async: false,
+        success: function(res) {
+            result = res;
+        }
+    });
+    return result;
+}
+
+
+
+
+const displayCartInfo = (data) => {
+    for (i = 0; i < data.length; i++) {
+        $('#cart').append(cartComponent(data[i]['product_name'], data[i]['cart_id'], data[i]['count_num'] * data[i]['product_price'], data[i]['count_num']));
+    }
+}
+
+const cartComponent = (product_name, cart_id, total_price, count_num) => {
+    return `
+    <div class="row align-items-center border-line py-3 cart${cart_id}">
+        <div class="col-6"><img src="./getImage.jfif" alt="" width="113.7" height="162.79"><h2 class="ms-5" style="display: inline;">${product_name}</h2></div>
+        <ul class="quanity">
+            <li><span class="sub" id="${cart_id}sub">-</span></li>
+            <li><input type="text" class="input-num" id="${cart_id}input-num" value="${count_num}"></li>
+            <li><span class="add" id="${cart_id}add">+</span></li>
+        </ul>
+        <div class="col-2 px-0" style="margin-left: 130px;"><h2 id="${cart_id}price">$${total_price}</h2></div>
+        <button type="button" class="btn-close" id="${cart_id}" aria-label="Close"></button>
+    </div>
+    `;
+}
+
+const removeCartCard = (id) => {
+    let className = ".cart" + id;
+    $(className).remove();
+    deleteCart(id);
+}
+
+const displayCouponInfo = (data) => {
+    for (i = 0; i < data.length; i++) {
+        $('#form-select').append(`<option class="couponOption" id="${data[i]['coupon_id']}coupon" value="${i + 1}">${data[i]['feature']}</option>`);
+    }
+}
+
+
+
+const add = (id) => {
+    let num = "#" + id + "input-num";
+    $(num).val(parseInt($(num).val()) + 1);
+    updateCart(id, $(num).val());
+    let id_price = "#" + id + "price";
+    let res = getCart();
+    for (i = 0; i < res.length; i++) {
+        if (res[i]['cart_id'] == id) {         
+            $(id_price).text("$" + $(num).val() * res[i]['product_price']);
+            getSubtotal(res, $(num).val() * res[i]['product_price'], i);
+            break;
+        }
+    }
+}
+const sub = (id) => {
+    let num = "#" + id + "input-num";
+    if(parseInt($(num).val()) <= 0) {
+        $(num).val(0);
+    } else {
+        $(num).val(parseInt($(num).val()) - 1);
+    }
+    updateCart(id, $(num).val());
+    let id_price = "#" + id + "price";
+    let res = getCart();
+    for (i = 0; i < res.length; i++) {
+        if (res[i]['cart_id'] == id) {         
+            $(id_price).text("$" + $(num).val() * res[i]['product_price']);
+            getSubtotal(res, $(num).val() * res[i]['product_price'], i);
+            break;
+        }
+    }
+}
+
+const getSubtotal = (data, nowProductPrice, i) => {
+    let subtotal = 0;
+    for (j = 0; j < data.length; j++) {
+        if (j != i) {
+            subtotal += data[j]['count_num'] * data[j]['product_price'];
+        }else {
+            subtotal += nowProductPrice;
+        }
+    }
+    $('#subtotal').text("$" + subtotal);
+    if (parseInt($('#discount').text().substring(1)) == 0) {
+        dis = parseInt($('#discount').text().substring(1));
+    } else {
+        dis = parseInt($('#discount').text().substring(2));
+    }
+    shi = parseInt($('#shipping').text().substring(1));
+    $('#total').text("$" + (subtotal + shi - dis));
+}
---
+++ view/src/script/cartLoad.js
@@ -0,0 +1,60 @@
+$(() => {
+    let resCart = getCart();
+    displayCartInfo(resCart);
+    getSubtotal(resCart, 0, -1);
+    let resCoupon = getCoupon();
+    displayCouponInfo(resCoupon);
+    let couponOption = document.getElementById('form-select');
+    couponOption.onchange=function() {
+        curCoupon = $('#form-select option:selected').text();
+        if (curCoupon == 'clear all') {
+            $('#displayCoupon1').remove();
+            $('#displayCoupon2').remove();
+            $('#discount').text("$0");
+            $('#shipping').text("$60");
+        } else if (curCoupon == '50' || curCoupon == '100' || curCoupon == '150') {
+            if ($('#displayCoupon1').text() && ($('#displayCoupon1').text() == 'free-shipping')) {
+                if (!$('#displayCoupon2').text()) {
+                    $('#displayCoupon').append(`<span id="displayCoupon2" class="display-coupon ms-2">${curCoupon}</span>`);
+                } else {
+                    $('#displayCoupon2').text(curCoupon);
+                }
+            } else if (!$('#displayCoupon1').text()) {
+                $('#displayCoupon').append(`<span id="displayCoupon1" class="display-coupon ms-2">${curCoupon}</span>`);
+            } else {
+                $('#displayCoupon1').text(curCoupon);
+            }
+            $('#discount').text("-$" + curCoupon);
+        } else if (curCoupon == 'free-shipping') {
+            if (!$('#displayCoupon1').text()) {
+                $('#displayCoupon').append(`<span id="displayCoupon1" class="display-coupon ms-2">${curCoupon}</span>`);
+            } else if ($('#displayCoupon1').text() && ($('#displayCoupon1').text() == 'free-shipping')) {
+            } else if (!$('#displayCoupon2').text()) {
+                $('#displayCoupon').append(`<span id="displayCoupon2" class="display-coupon ms-2">${curCoupon}</span>`);
+            }
+            $('#shipping').text("$0");
+        }
+        if (parseInt($('#discount').text().substring(1)) == 0) {
+            dis = parseInt($('#discount').text().substring(1));
+        } else {
+            dis = parseInt($('#discount').text().substring(2));
+        }
+        shi = parseInt($('#shipping').text().substring(1));
+        subt = parseInt($('#subtotal').text().substring(1));
+        $('#total').text("$" + (subt + shi - dis));
+    };
+    
+    $(".btn-close").click(function() {
+        let id = $(this).attr('id');
+        removeCartCard(id);
+    });
+    $('.add').click(function() {
+        let id = parseInt($(this).attr('id'));
+        add(id);
+    });
+    $('.sub').click(function() {
+        let id = parseInt($(this).attr('id'));
+        sub(id);
+    });
+    checkout();
+});
