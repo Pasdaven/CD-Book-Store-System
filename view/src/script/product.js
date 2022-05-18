edb4615a8c207308da9270014d755a2fe95c76e5
David Ho <davidho0403@gmail.com>
2022-05-19 01:45:14 +0800

    feat: displayRate()
    此函式可以將評分轉換為星星數並顯示

--- view/src/script/product.js
+++ view/src/script/product.js
@@ -281,3 +281,28 @@ const createBrowsingHistory = () => {
         success: (res) => console.log(res),
     });
 };
+
+const displayRate = (data) => {
+    let rate = parseInt(data[0]["avg_star"][0]["AVG(star)"]);
+    switch (rate) {
+        case 1:
+            $(".star1").addClass("bi-star-fill");
+            $(".star2, .star3, .star4, .star5").addClass("bi-star");
+            break;
+        case 2:
+            $(".star1, .star2").addClass("bi-star-fill");
+            $(".star3, .star4, .star5").addClass("bi-star");
+            break;
+        case 3:
+            $(".star1, .star2, .star3").addClass("bi-star-fill");
+            $(".star4, .star5").addClass("bi-star");
+            break;
+        case 4:
+            $(".star1, .star2, .star3, .star4").addClass("bi-star-fill");
+            $(".star5").addClass("bi-star");
+            break;
+        case 5:
+            $(".star1, .star2, .star3, .star4, .star5").addClass("bi-star-fill");
+            break;
+    }
+};
