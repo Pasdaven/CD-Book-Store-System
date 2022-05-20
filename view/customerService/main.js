fd757def2ca9ad0b5e4b66d2d3d42d8170b0f816
David Ho <davidho0403@gmail.com>
2022-05-21 00:52:59 +0800

    feat: js of cs
    和cs.js內容相同

---
+++ view/customerService/main.js
@@ -0,0 +1,177 @@
+$(() => {
+    loadOrder();
+    loadMsg();
+    $("#send-btn").click(() => {
+        createCsMessage();
+        cleanInputBox();
+    });
+    $("#msg-content").keypress((e) => {
+        code = e.keyCode ? e.keyCode : e.which;
+        if (code == 13) {
+            createCsMessage();
+            cleanInputBox();
+        }
+    });
+    setInterval(() => {
+        loadUnreadMsg();
+    }, 500);
+});
+
+const loadOrder = () => {
+    let cs_record_id = 1;
+    let data = {
+        controller: "customerService",
+        method: "searchOrderInfoByCsRecId",
+        parameter: {
+            cs_record_id: cs_record_id,
+        },
+    };
+    let json = JSON.stringify(data);
+    $.ajax({
+        url: "/cd-book-store-system/controller/core.php",
+        method: "POST",
+        data: json,
+        success: (res) => {
+            console.log(res);
+            displayOrderInfo(res);
+        },
+    });
+};
+
+const createProductComponent = (data) => {
+    let html = `
+        <div class="col-12 product-component">
+            <div class="product-img">
+                <img src="${data['product_info'][0]['product_image']}">
+            </div>
+            <div class="product-info">
+                <h1>${data['product_info'][0]['product_name']}</h1>
+                <a>Count:</a> ${data['count_num']}<br>
+                <a>Price:</a> ${data['product_info'][0]['product_price']}<br>
+            </div>
+        </div>
+    `
+    $('#product_component_area').append(html);
+}
+
+const displayOrderInfo = (res) => {
+    $('#order_id').html(res[0]['order_id']);
+    $('#order_state').html(res[0]['order_state']);
+    $('.product-order-state').addClass(res[0]['order_state']);
+    if (res[0]['order_state'] == 'wait') {
+        $('#state_icon').addClass('bi-hourglass-top');
+    }
+    if (res[0]['order_state'] == 'finish') {
+        $('#state_icon').addClass('bi-check-circle');
+    }
+    if (res[0]['order_state'] == 'cancel') {
+        $('#state_icon').addClass('bi-x-circle');
+    }
+    if (res[0]['order_state'] == 'return') {
+        $('#state_icon').addClass('bi-truck');
+    }
+    res['product'].forEach((element) => {
+        createProductComponent(element);
+    });
+}
+
+const createCsMessage = () => {
+    if ($("#msg-content").val() != "") {
+        let cs_record_id = 1;
+        let msg_content = $("#msg-content").val();
+        let msg_by = "member";
+        let data = {
+            controller: "customerService",
+            method: "createCsMessage",
+            parameter: {
+                cs_record_id: cs_record_id,
+                msg_content: msg_content,
+                msg_by: msg_by,
+            },
+        };
+        let json = JSON.stringify(data);
+        $.ajax({
+            url: "/cd-book-store-system/controller/core.php",
+            method: "POST",
+            data: json,
+            success: (res) => {
+                console.log(res);
+                createMsgComponent(res[0]);
+                scrollToBottom();
+            },
+        });
+    }
+};
+
+const cleanInputBox = () => {
+    $("#msg-content").val("");
+};
+
+const createMsgComponent = (data) => {
+    let msg_by = data["msg_by"] == "member" ? "by-self" : "by-other";
+    let html = `
+        <div class="col-12 msg-component ${msg_by}">
+            <div class="msg-component-body">
+                ${data["msg_content"]}
+            </div>
+        </div>
+    `;
+    $("#msg-content-area").append(html);
+};
+
+const loadUnreadMsg = () => {
+    let cs_record_id = 1;
+    let msg_by = "cs";
+    let data = {
+        controller: "customerService",
+        method: "searchUnreadMsg",
+        parameter: {
+            cs_record_id: cs_record_id,
+            msg_by: msg_by,
+        },
+    };
+    let json = JSON.stringify(data);
+    $.ajax({
+        url: "/cd-book-store-system/controller/core.php",
+        method: "POST",
+        data: json,
+        success: (res) => {
+            if (res != "") {
+                res.forEach((element) => {
+                    createMsgComponent(element);
+                });
+                scrollToBottom();
+            }
+        },
+    });
+};
+
+const loadMsg = () => {
+    let cs_record_id = 1;
+    let data = {
+        controller: "customerService",
+        method: "searchMsgByCsRecId",
+        parameter: {
+            cs_record_id: cs_record_id,
+        },
+    };
+    let json = JSON.stringify(data);
+    $.ajax({
+        url: "/cd-book-store-system/controller/core.php",
+        method: "POST",
+        data: json,
+        success: (res) => {
+            if (res != "") {
+                res.forEach((element) => {
+                    createMsgComponent(element);
+                });
+                scrollToBottom();
+            }
+        },
+    });
+};
+
+const scrollToBottom = () => {
+    let height = $("#msg-content-area")[0].scrollHeight;
+    $("#msg-content-area").scrollTop(height);
+};
