badfc7d5427144cafae928a91f35bb409726a6f8
Pascal <a306090603@gmail.com>
2022-05-21 13:54:23 +0800

    new timelySearch function

--- view/src/script/searchProduct.js
+++ view/src/script/searchProduct.js
@@ -4,6 +4,43 @@ $('#search_btn').click(function() {
     let url = 'http://localhost/CD-BOOK-STORE-SYSTEM/view/searchProduct/?productName=' + val;
     window.location.replace(url);
 });
+document.getElementById('search').oninput=function() {
+    $('#search-list-group').remove();
+    $('#search-div').append(`<div class="list-group" id="search-list-group" style="position: fixed;width:240px;"></div>`)
+    product_name = $('#search').val();
+    if (product_name != "") {
+        res = timelySearchProduct(product_name);
+        displayTimelysearchProduct(res);
+    }
+};
 
+const timelySearchProduct = (product_name) => {
+    let data = {
+        controller: 'product',
+        method: 'timelysearchProductByName',
+        parameter: {
+            product_name: product_name
+        }
+    };
+    let json = JSON.stringify(data);
+    $.ajax({
+        url: '/CD-BOOK-STORE-SYSTEM/controller/core.php',
+        method: 'POST',
+        data: json,
+        async: false,
+        success: function (res) {
+            result = res;
+        },
+        error: function (res) {
+            result = 0;
+        }
+    });
+    return result;
+}
 
+const displayTimelysearchProduct = (data) => {
+    for (let i = 0; i < data.length; i++) {
+        $('#search-list-group').append(`<a href="http://localhost/CD-BOOK-STORE-SYSTEM/view/searchProduct/?productName=${data[i]['product_name']}" class="list-group-item list-group-item-action">${data[i]['product_name']}</a>`);
+    }
+}
 
