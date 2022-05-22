1af810555fe3d259a96492d8ba0031ebdda35be8
David Ho <davidho0403@gmail.com>
2022-05-23 02:10:21 +0800

    feat: displayNoOrder()
    此函式可以在該會員沒有任何訂單記錄時顯示資訊

--- view/cs/main.js
+++ view/cs/main.js
@@ -99,3 +99,8 @@ const jumpChatRoom = (order_id) => {
     });
 }
 
+const displayNoOrder = () => {
+    console.log('hu');
+    let html = "<p class='text-center'>You don't have any order yet.</p>";
+    $('#card-area').html(html);
+}
