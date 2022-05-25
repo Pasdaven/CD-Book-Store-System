9274529850d2592074a76d98eb7668066922489e
David Ho <davidho0403@gmail.com>
2022-05-25 16:11:01 +0800

    feat: add website icon

--- view/cart/index.php
+++ view/cart/index.php
@@ -19,6 +19,7 @@ if (isset($_SESSION['member_id']) && isset($_SESSION['email'])) {
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="/CD-Book-Store-System/view/src/style/cart.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css" />
+        <link rel="shortcut icon" href="/CD-Book-Store-System/view/src/image/logo.png" type="image/x-icon" />
     </head>
 
     <body>
--- view/cs/csChat/index.php
+++ view/cs/csChat/index.php
@@ -17,6 +17,7 @@ if (isset($_SESSION['member_id']) && isset($_SESSION['email'])) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
         <!-- Custom CSS -->
         <link rel="stylesheet" href="style.css" />
+        <link rel="shortcut icon" href="/CD-Book-Store-System/view/src/image/logo.png" type="image/x-icon" />
         <title>Member CS</title>
     </head>
 
