8d1987cd6f625b826921672b7f9a54971167de78
ridemountainpig <lin0905580802@gmail.com>
2022-05-31 23:30:14 +0800

    feat : send coupon in different condition

--- controller/coupon.php
+++ controller/coupon.php
@@ -10,14 +10,32 @@ class Coupon extends Model {
 
     // 發送免運券給信用分數大於80的會員
     public function sendFreeShippingCoupon() {
-        $member = new Member();
-
-        $memberList = $member->getMemberCredit();
         $month = date("Y-m");
+        if (count($this->execute($this->select($this->table) . $this->where('coupon_month', '=', $month))) != 0) {
+            return false;
+        }
+
+        $member = new Member();
+        $memberList = $member->getMemberCredit();
         foreach ($memberList as $value) {
             $sql = $this->insert(['member_id' => $value['member_id'], 'feature' => 'free-shipping', 'coupon_month' => $month]);
             $this->execute($sql);
         }
+
+        $year = date("Y");
+        $month = date("m");
+        $sql = 'SELECT member_id, SUM(price) FROM order_list WHERE order_year = ' . $year . ' and order_month = ' . $month . ' GROUP BY member_id';
+        $orderList = $this->execute($sql);
+        for ($i = 0; $i < count($orderList); $i++) {
+            if ($orderList[$i]['SUM(price)'] > 5000) {
+                $this->execute($this->insert(['member_id' => $orderList[$i]['member_id'], 'feature' => '150', 'coupon_month' => date("Y-m")]));
+            } else if ($orderList[$i]['SUM(price)'] > 3000 && $orderList[$i]['SUM(price)'] < 4999) {
+                $this->execute($this->insert(['member_id' => $orderList[$i]['member_id'], 'feature' => '100', 'coupon_month' => date("Y-m")]));
+            } else if ($orderList[$i]['SUM(price)'] > 1000 && $orderList[$i]['SUM(price)'] < 2999) {
+                $this->execute($this->insert(['member_id' => $orderList[$i]['member_id'], 'feature' => '50', 'coupon_month' => date("Y-m")]));
+            }
+        }
+        return true;
     }
 
     // 查詢某個member_id擁有的coupon
