ad9af3df680e1a8b89c7da67c679b4122ad0bb7d
Pascal <a306090603@gmail.com>
2022-06-04 20:50:25 +0800

    update text with  upload successfully

--- controller/addProduct.php
+++ controller/addProduct.php
@@ -70,6 +70,23 @@ if (
 
 
 if ($uploadOk != 0 && move_uploaded_file($_FILES["product_image"]["tmp_name"], $target_file)) {
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+
+    <body>
+        <h1>Upload successfully.</h1>
+    </body>
+
+    </html>
+<?php
     $_POST['product_image'] = "/CD-Book-Store-System/view/src/image/book/" . basename($_FILES["product_image"]["name"]);
     $product = new Product();
     $product->createProduct($_POST);
